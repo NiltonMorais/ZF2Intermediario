eac98a1cbaaeadb1ac7dc167bd32f931428bf258
Nilton Morais <niltonmorais123@gmail.com>
2015-11-07 14:06:03 -0200

    02 - configurando module.config

--- module/SONUser/config/module.config.php
+++ module/SONUser/config/module.config.php
@@ -30,12 +30,28 @@ return array(
         'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
         ),
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__.'_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__.'/../src/'.__NAMESPACE__.'Entity')
+            ),
+            'orm_default' => array(
+                'drivers'   => array(
+                    __NAMESPACE__.'\Entity' => __NAMESPACE__.'_driver'
+                ),                
+            ),
+        ),
+    ),
+    'data-fixture'  => array(
+        'SONUser_fixture' => __DIR__.'/../src/SONUser/Fixture',
     ),
 );
