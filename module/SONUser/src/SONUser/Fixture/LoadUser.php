55ea86f0b02c727aee11ff1ba41880e9b628eae0
Nilton Morais <niltonmorais123@gmail.com>
2015-11-20 00:52:30 -0200

    05 - Criando Fixture

---
+++ module/SONUser/src/SONUser/Fixture/LoadUser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SONUser\Fixture;
+
+use Doctrine\Common\DataFixtures\AbstractFixture,
+    Doctrine\Common\Persistence\ObjectManager;
+
+use SONUser\Entity\User;
+
+class LoadUser extends AbstractFixture{
+    
+    public function load(ObjectManager $manager) {
+        $user = new User();
+        $user->setNome("Nilton")
+                ->setEmail("nilton.morais@avatim.com.br")
+                ->setPassword("123456")
+                ->setActive(true);
+        
+        $manager->persist($user);
+        
+        $manager->flush();        
+    }
+
+}
