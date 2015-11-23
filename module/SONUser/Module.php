40e4f4b081012ae58e33c9ec960b83e914e627e8
Nilton Morais <niltonmorais123@gmail.com>
2015-11-23 00:56:52 -0200

    03 - Registrando serviços

--- module/SONUser/Module.php
+++ module/SONUser/Module.php
@@ -1,6 +1,9 @@
 <?php
 namespace SONUser;
 
+use Zend\Mail\Transport\Smtp as SmtpTransport;
+use Zend\Mail\Transport\SmtpOptions;
+
 class Module
 {
     public function getConfig()
@@ -16,6 +19,27 @@ class Module
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
                 ),
             ),
+        );
+    }
+    
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'SONUser\Mail\Transport' => function($sm){
+                    $config = $sm->get('Config');
+                    $transport = new SmtpTransport;
+                    $options = new SmtpOptions($config['mail']);
+                    $transport->setOptions($options);
+                    
+                    return $transport;
+                },
+                 'SONUser\Service\User' => function($sm){
+                    return new Service\User($sm->get('Doctrine\ORM\EntityManager'),
+                                               $sm->get('SONUser\Mail\Transport'),
+                                               $sm->get('View'));
+                 }
+            )
         );
     }
 }
