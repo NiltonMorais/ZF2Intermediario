7f01a0cb9b36e0f6d5207bed654f6109043e393c
Nilton Morais <niltonmorais123@gmail.com>
2015-11-23 11:43:01 -0200

    03 - Implementando action

--- module/SONUser/src/SONUser/Controller/IndexController.php
+++ module/SONUser/src/SONUser/Controller/IndexController.php
@@ -1,11 +1,36 @@
 <?php
 namespace SONUser\Controller;
+
 use Zend\Mvc\Controller\AbstractActionController,
     Zend\View\Model\ViewModel;
 
+use SONUser\Form\User as FormUser;
+
 class IndexController extends AbstractActionController{
     
-    public function registerAction(){
-        return new ViewModel();
+    public function registerAction()
+    {
+        $form = new FormUser();
+        $request = $this->getRequest();
+        
+        if($request->isPost()){
+            $form->setData($request->getPost());
+            if($form->isValid()){
+                $service = $this->getServiceLocator()->get('SONUser\Service\User');
+                if($service->insert($request->getPost()->toArray())){
+                    $fm = $this->flashMessenger()
+                            ->setNamespace('SONUser')
+                            ->addSuccessMessage("Usuário cadastrado com sucesso!");
+                }
+                
+                return $this->redirect()->toRoute('sonuser-register');
+            }
+        }
+        
+        $messages = $this->flashMessenger()
+                ->setNamespace('SONUser')
+                ->getMessages();
+        
+        return new ViewModel(array('form' => $form, 'messages' => $messages));
     }
 }
