2e4a018c48e1013c8798cf3a8f7f13a92fdc33f8
Nilton Morais <niltonmorais123@gmail.com>
2015-11-22 22:21:26 -0200

    02 - Criando class Mail

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,16 +1,15 @@
 <?php
-/**
- * Global Configuration Override
- *
- * You can use this file for overriding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
- *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
- */
-
 return array(
-    // ...
+    'mail' => array(
+        'name' => 'smtp.googlemail.com',
+        'host' => 'smtp.googlemail.com',
+        'connection_class' => 'login',
+        'connection_config' => array(
+            'username' => 'teste@gmail.com',
+            'password' => '123',
+            'ssl'   =>  'tls',
+            'port'  =>  465,
+            'from'  => 'nilton_even@hotmail.com'            
+        )
+    )
 );
