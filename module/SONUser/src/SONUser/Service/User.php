cb843109573a947e5c5320635c41acf115f0fadd
Nilton Morais <niltonmorais123@gmail.com>
2015-11-23 00:37:22 -0200

    01 - Inserindo e enviando email

---
+++ module/SONUser/src/SONUser/Service/User.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SONUser\Service;
+
+use Doctrine\ORM\EntityManager;
+
+use Zend\Stdlib\Hydrator;
+use Zend\Mail\Transport\Smtp as SmtpTransport;
+use SONBase\Mail\Mail;
+
+class User extends AbstractService{
+    
+    protected $transport;
+    protected $view;
+    
+    public function __construct(EntityManager $em, SmtpTransport $transport, $view)
+    {
+        parent::__construct($em);
+        
+        $this->entity = "SONUser\Entity\User";
+        $this->transport = $transport;
+        $this->view = $view;
+    }
+    
+    public function insert(array $data){
+        $entity = parent::insert($data);
+        
+        $dataEmail = array('nome' => $data['nome'],'activationKey' => $entity->getActivationKey());
+        
+        if($entity){
+            $mail = new Mail($this->transport, $this->view, 'add-user');
+            $mail->setSubject('Confirmação de cadastro')
+                    ->setTo($data['email'])
+                    ->setData($dataEmail)
+                    ->prepare()
+                    ->send();
+            
+            return $entity;
+        }
+            
+    }
+    
+}
