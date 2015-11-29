a49116e816e041f8a2dc3ff4cb2b9abef39e7816
Nilton Morais <niltonmorais123@gmail.com>
2015-11-29 20:27:37 -0300

    11 -  newAction no UsersController

--- module/SONUser/src/SONUser/Controller/UsersController.php
+++ module/SONUser/src/SONUser/Controller/UsersController.php
@@ -13,4 +13,32 @@ class UsersController extends CrudController {
        $this->route = "sonuser-admin";
     }
     
+     public function editAction()
+    {
+        $id = $this->params()->fromRoute('id', 0);
+        $form = new $this->form();
+        $request = $this->getRequest();        
+        
+        if($id){
+            $repository = $this->getEm()->getRepository($this->entity);
+            $entity = $repository->find($id);
+            $array = $entity->toArray();
+            unset($array['password']);
+            $form->setData($array);
+        }
+        
+         if($request->isPost()){
+            $form->setData($request->getPost());
+            
+            if($form->isValid()){
+                $service = $this->getServiceLocator()->get($this->service);
+                $service->update($request->getPost()->toArray());
+                
+                return $this->redirect()->toRoute($this->route, array("controller" => $this->controller));
+            }
+        }
+        
+        return new ViewModel(array("form" => $form));
+    }
+    
 }
