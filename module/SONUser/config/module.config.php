265be2eae2e47086b72d003ec5ac5e32b24b16bd
Nilton Morais <niltonmorais123@gmail.com>
2015-11-29 16:19:21 -0300

    01 - Configurando rota

--- module/SONUser/config/module.config.php
+++ module/SONUser/config/module.config.php
@@ -26,6 +26,50 @@ return array(
                     )
                 )
             )
+        ),
+        "sonuser-admin" => array(
+            "type" => "Literal",
+            "options"   => array(
+                "route" => "/admin",
+                "defaults"  => array(
+                    '__NAMESPACE__' => "SONUser\Controller",
+                    "controller" => "Users",
+                    "action"    => "index"
+                )
+            ),
+            "may_terminate" => true,
+            "child_routes" => array(
+                "default" => array(
+                    "type" => "Segment",
+                    "options"   => array(
+                        "route" => "/[:controller[:action[:id]]]",
+                        "constraints" => array(
+                            "controller" => "[a-zA-Z][a-zA-Z0-9_-]*",
+                            "action" => "[a-zA-Z][a-zA-Z0-9_-]*",
+                            "id" => "\d+"
+                        ),
+                        "defaults" => array(
+                            "__NAMESPACE__" => "SONUser\Controller",
+                            "controller"    => "Users"
+                        )
+                    )
+                ),
+                "paginator" => array(
+                    "type"  => "Segment",
+                    "options"   => array(
+                        "route" => "/[:controller/page/:page]]",
+                        "constraints" => array(
+                            "controller" => "[a-zA-Z][a-zA-Z0-9_-]*",
+                            "action" => "[a-zA-Z][a-zA-Z0-9_-]*",
+                            "page" => "\d+"
+                        ),
+                        "defaults" => array(
+                            "__NAMESPACE__" => "SONUser\Controller",
+                            "controller" => "Users"
+                        )
+                    )
+                )
+            )
         )
     ),
     'controllers' => array(
