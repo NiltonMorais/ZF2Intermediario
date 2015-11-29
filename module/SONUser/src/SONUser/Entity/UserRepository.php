78ce55ff1ec7c7fdcbdf762df4cafa3b6302a6b7
Nilton Morais <niltonmorais123@gmail.com>
2015-11-29 20:59:06 -0300

    02 - Criando metodo no repository

--- module/SONUser/src/SONUser/Entity/UserRepository.php
+++ module/SONUser/src/SONUser/Entity/UserRepository.php
@@ -3,6 +3,18 @@ namespace SONUser\Entity;
 
 use Doctrine\ORM\EntityRepository;
 
-class UserRepository extends EntityRepository{
-   
+class UserRepository extends EntityRepository
+{
+   public function findByEmailAndPassword($email, $password)
+   {
+       $user = $this->findOneByEmail($email);
+       
+       if($user){
+           $hashSenha = $user->encryptPassword($password);
+           if($hashSenha == $user->getPassword()){
+               return $user;
+           }
+       }
+       return false;
+   }
 }
